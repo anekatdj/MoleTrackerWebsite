76ff14f735bb079945b2996bc037483e7444e2fb
jacob0978 <den1761@myspot.dk>
2021-03-19 13:22:17 +0100

    Test med fejl

---
+++ MoleTrackerWebsite/Data/LoginData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DTO
+{
+    public class LoginDataOLD
+    {
+        [Required]
+        [MinLength(1, ErrorMessage = "Username is too short")]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string UserName { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "password is too short")]
+        public string password { get; set; }
+
+    }
+}
