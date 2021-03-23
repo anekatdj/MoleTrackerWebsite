0b36d0d4c9479d613c8aaad3593feb40408958e9
laurabirchx <57993207+laurabirchx@users.noreply.github.com>
2021-03-23 09:32:19 +0100

    staged

--- LogicLayer/LogInController.cs
+++ LogicLayer/LogInController.cs
@@ -8,17 +8,17 @@ namespace LogicLayer
     {
         public bool HandleLogin(LoginInfoDTO loginInfo)
         {
-            //return true;
-            {
-                if (loginInfo.Username == "121212-1212" && loginInfo.Password == "kodeord")
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
+            return true;
+            //{
+            //    if (loginInfo.Username == "121212-1212" && loginInfo.Password == "kodeord")
+            //    {
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            //}
         }
     }
 }
