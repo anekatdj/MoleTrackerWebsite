5caf146810e3175c7f27d46ab708b78cb4894411
JacobJH <60734597+JacobJH@users.noreply.github.com>
2021-04-06 09:29:25 +0200

    Login binding

---
+++ BusinessLogicLayer/BusinessLogicLayerInterfaces/ILoadPatients.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DTO.LoginDTO;
+
+namespace BusinessLogicLayer.BusinessLogicLayerInterfaces
+{
+    public interface ILoadPatients
+    {
+        void LoadPatientList(DoctorInfoDTO doctorInfo);
+    }
+}
---
+++ BusinessLogicLayer/LoadPatients.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessLogicLayer.BusinessLogicLayerInterfaces;
+using DTO.LoginDTO;
+
+namespace BusinessLogicLayer
+{
+    public class LoadPatients : ILoadPatients
+    {
+        public void LoadPatientList(DoctorInfoDTO doctorInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
