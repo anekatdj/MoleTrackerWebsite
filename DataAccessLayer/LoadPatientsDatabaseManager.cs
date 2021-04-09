90bcf33a8f460363c6d27a073e3bb9b1ddef80b6
jacob0978 <den1761@myspot.dk>
2021-04-09 10:16:54 +0200

    Datalag til UC2 og UC3

--- DataAccessLayer/LoadPatientsDatabaseManager.cs
+++ DataAccessLayer/LoadPatientsDatabaseManager.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 using DataAccessLayer.DataAccessLayerInterfaces;
+using APIWebServiesConnector;
+using DTO.MISCDTO;
+using DTO.LoginDTO;
 
 namespace DataAccessLayer
 {
     public class LoadPatientsDatabaseManager : ILoadPatientsDatabaseManager
     {
+        private MedicalPracticePatientsDTO medicalPracticePatients;
+        private IAPIService API;
+
+        public LoadPatientsDatabaseManager()
+        {
+            API = new StubApiService();
+        }
+
+        public MedicalPracticePatients GetMedicalPracticePatients()
+        {
+            DoctorInfoDTO doctorInfoDTO = new DoctorInfoDTO();
+            try
+            {
+                medicalPracticePatients = API.GetObject<MedicalPracticePatientsDTO, DoctorInfoDTO>("GetMedicalPracticePatients", doctorInfoDTO);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return medicalPracticePatients;
+        }
     }
 }
--- DataAccessLayer/SelectPatientDatabaseManager.cs
+++ DataAccessLayer/SelectPatientDatabaseManager.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using DataAccessLayer.DataAccessLayerInterfaces;
+using APIWebServiesConnector;
+using DTO.MISCDTO;
 
 namespace DataAccessLayer
 {
     public class SelectPatientDatabaseManager : ISelectPatientDatabaseManager
     {
-
+        
     }
 }
