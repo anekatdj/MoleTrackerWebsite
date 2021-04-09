e4cffd9e25f76574d37d29ef43db0ecee2199a1f
jacob0978 <den1761@myspot.dk>
2021-04-09 11:48:01 +0200

    Ingen fejl databasemanager for UC2-3

--- DataAccessLayer/SelectPatientDatabaseManager.cs
+++ DataAccessLayer/SelectPatientDatabaseManager.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Text;
 using DataAccessLayer.DataAccessLayerInterfaces;
 using APIWebServiesConnector;
+using DataClasses.MISCDTO;
+using DataClasses.Domain;
 
 namespace DataAccessLayer
 {
     public class SelectPatientDatabaseManager : ISelectPatientDatabaseManager
     {
-        
+        private PatientInfoRequestDTO patientInfoRequestDTO;
+        private PatientData patientData;
+        private IAPIService API;
+
+        public SelectPatientDatabaseManager()
+        {
+            API = new StubApiService();
+        }
+
+        public PatientData GetPatientDate(PatientInfoRequestDTO patientInfoRequestDTO)
+        {
+            PatientDataDTO patientDataDTO = new PatientDataDTO();
+            
+            try
+            {
+                patientDataDTO = API.GetObject<PatientDataDTO, PatientInfoRequestDTO>("GetPatientData", patientInfoRequestDTO);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            patientData = new PatientData()
+            {
+                PatientID = patientDataDTO.PatientID,
+                CollectionList = patientDataDTO.CollectionList
+            };
+            return patientData;
+        }
     }
 }
