805023454757c4b3965761439e1f5d7166a28876
anekatdj <60734487+anekatdj@users.noreply.github.com>
2021-04-21 13:13:48 +0200

    UnitTest på LoginPage

--- MoleTrackerWebsite_PW.UnitTests/UT_LogInPage.cs
+++ MoleTrackerWebsite_PW.UnitTests/UT_LogInPage.cs
@@ -1,27 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Bunit;
 using MoleTrackerWebsite.Pages;
+using NSubstitute;
 using NUnit.Framework;
+using TestContext = Bunit.TestContext;
 
 
 namespace MoleTrackerWebsite_PW.UnitTests
 {
     public class UT_LogInPage
     {
-        private LogInPage _uut;
+        private LogInPage _logInPage;
+        //private PatientLogInPage _patientLogInPage;
 
         [SetUp]
         public void Setup()
         {
-            _uut = new LogInPage();
-
+            _logInPage = Substitute.For<LogInPage>();
+            //_uut = new LogInPage();
+            //_patientLogInPage = Substitute.For<PatientLogInPage>(); //Ups man må kun med interfaces
         }
 
         [Test]
         public void CallNavigateToPatientLoginPageMethod()
         {
+            using var ctx = new TestContext();
 
+            var cut = ctx.RenderComponent<LogInPage>();
+
+            cut.Find("button").Click();
+
+            //_logInPage.Received(1).NavigateToPatientLogInPage();
+            //_patientLogInPage.Received(1).
         }
     }
 }
