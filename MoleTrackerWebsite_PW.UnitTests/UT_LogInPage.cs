764f9ad6c12131582cd7a3590fba8fa1670ebc4a
anekatdj <60734487+anekatdj@users.noreply.github.com>
2021-04-21 11:51:51 +0200

    LogInController and LogInPage UnitTest begyndelse

---
+++ MoleTrackerWebsite_PW.UnitTests/UT_LogInPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MoleTrackerWebsite.Pages;
+using NUnit.Framework;
+
+
+namespace MoleTrackerWebsite_PW.UnitTests
+{
+    public class UT_LogInPage
+    {
+        private LogInPage _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new LogInPage();
+
+        }
+
+        [Test]
+        public void CallNavigateToPatientLoginPageMethod()
+        {
+
+        }
+    }
+}
