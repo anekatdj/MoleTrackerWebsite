76e978bdb51f249acc026694d694bbcfa4db922f
anekatdj <60734487+anekatdj@users.noreply.github.com>
2021-04-23 14:37:06 +0200

    Arbejde på UnitTest til LoginPage

--- MoleTrackerWebsite_PW.UnitTests/UT_LogInPage.cs
+++ MoleTrackerWebsite_PW.UnitTests/UT_LogInPage.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Bunit;
+using Bunit.TestDoubles;
+using Microsoft.AspNetCore.Components;
 using MoleTrackerWebsite.Pages;
 using NSubstitute;
 using NUnit.Framework;
@@ -13,27 +15,63 @@ namespace MoleTrackerWebsite_PW.UnitTests
     public class UT_LogInPage
     {
         private LogInPage _logInPage;
+
+        private NavigationManager _navigation;
         //private PatientLogInPage _patientLogInPage;
 
         [SetUp]
         public void Setup()
         {
-            _logInPage = Substitute.For<LogInPage>();
-            //_uut = new LogInPage();
-            //_patientLogInPage = Substitute.For<PatientLogInPage>(); //Ups man må kun med interfaces
+            _logInPage = new LogInPage();
+            //_logInPage = Substitute.For<LogInPage>();
+            _navigation = Substitute.For<NavigationManager>();
         }
 
         [Test]
         public void CallNavigateToPatientLoginPageMethod()
         {
+            //Arrange
             using var ctx = new TestContext();
 
+            //Act
             var cut = ctx.RenderComponent<LogInPage>();
 
-            cut.Find("button").Click();
+            cut.Find("#PatLogInButton").Click();
+
+            //Assert
+            //TODO Hvad skal der stå her?
+
+            //string url = "/patientLogin";
+
+            //_navigation.NavigateTo(Arg.Is<string>(str => str.Contains(url)));
+
+
+            /*_navigate.OutputLine(Arg.Is<string>(str => str.Contains($"{power}")))*/;
+
+
+
+            //_logInPage.NavigateToPatientLogInPage();
+
+            //_logInPage.Received(1).NavigateToPatientLogInPage();
+            //_navigation.Received(1).NavigateTo("/patientLogin");
 
             //_logInPage.Received(1).NavigateToPatientLogInPage();
             //_patientLogInPage.Received(1).
+        }
+
+        [Test]
+        public void CallNavigateToMedLoginPageMethod()
+        {
+            //Arrange
+            using var ctx = new TestContext();
+
+            //Act
+            var cut = ctx.RenderComponent<LogInPage>();
+
+            cut.Find("#MedLogInButton").Click();
+
+            //Assert
+            
         }
     }
 }
