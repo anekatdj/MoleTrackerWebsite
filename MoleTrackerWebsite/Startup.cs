58d98f932d7b26eff3cc8f7cd4e2804a6d8927a5
laurabirchx <57993207+laurabirchx@users.noreply.github.com>
2021-04-27 10:11:12 +0200

    arbejde med collectionpage

--- MoleTrackerWebsite/Startup.cs
+++ MoleTrackerWebsite/Startup.cs
@@ -37,7 +37,8 @@ namespace MoleTrackerWebsite
             services.AddSingleton<PatientInfo>();
             services.AddSingleton<Collection>();
             services.AddSingleton<DoctorInfo>();
-
+            services.AddSingleton<LocationOnBody>();
+            services.AddSingleton<PictureInfo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
