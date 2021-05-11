acc7c1dbea5eab0dc4ab0e69009492ed5b05883c
anekatdj <60734487+anekatdj@users.noreply.github.com>
2021-05-11 19:59:11 +0200

    Tilføjet APIFactory

---
+++ PW_DataAccessLayer/APIFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using APIWebServiesConnector;
+using APIWebServiesConnector.APIStringFabrics;
+
+namespace DataAccessLayer
+{
+    public static class APIFactory
+    {
+        static private IAPIService _api;
+
+        static private IAPIService API
+        {
+            get
+            {
+                if (_api == null)
+                {
+                    _api = new ApiService(APIStringFabric.GetDeveloperAPIString(), 'P');
+                }
+
+                return _api;
+            }
+        }
+
+        public static IAPIService GetAPI(string stub)
+        {
+            if (stub == "stub")
+            {
+                return new StubApiService();
+            }
+            else
+            {
+                return API;
+            }
+        }
+    }
+}
+
