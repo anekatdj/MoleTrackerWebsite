a74645a25d6eefaf95ae739e57073da51e06d91d
anekatdj <60734487+anekatdj@users.noreply.github.com>
2021-05-17 12:45:28 +0200

    DeleteCollectionDatabaseManager metode lavet

--- PW_DataAccessLayer/DeleteCollectionDatabaseManager.cs
+++ PW_DataAccessLayer/DeleteCollectionDatabaseManager.cs
@@ -1,8 +1,28 @@
-﻿using PW_DataAccessLayer.Interfaces;
+﻿using APIWebServiesConnector;
+using DataAccessLayer;
+using DataClasses.Domain.Collections;
+using DataClasses.Domain.Login;
+using DataClasses.DTO;
+using PW_DataAccessLayer.Interfaces;
 
 namespace PW_DataAccessLayer
 {
     public class DeleteCollectionDatabaseManager : IDeleteCollectionDatabaseManager
     {
+        private IAPIService API;
+
+        public DeleteCollectionDatabaseManager()
+        {
+            API = APIFactory.GetAPI("");
+        }
+
+        public void DeleteCollection(Collection collection, PatientInfo patientInfo)
+        {
+            CollectionRequestDTO deleteCollectionRequest = new CollectionRequestDTO();
+            deleteCollectionRequest.CollectionID = collection.CollectionID;
+            deleteCollectionRequest.PatientID = patientInfo.PatientID;
+
+            string result = API.PostObject<CollectionRequestDTO>("DeleteCollection", deleteCollectionRequest);
+        }
     }
 }
--- PW_DataAccessLayer/DeletePictureDatabaseManager.cs
+++ PW_DataAccessLayer/DeletePictureDatabaseManager.cs
@@ -1,8 +1,21 @@
-﻿using PW_DataAccessLayer.Interfaces;
+﻿using APIWebServiesConnector;
+using DataAccessLayer;
+using PW_DataAccessLayer.Interfaces;
 
 namespace PW_DataAccessLayer
 {
     class DeletePictureDatabaseManager : IDeletePictureDatabaseManager
     {
+        private IAPIService API;
+
+        public DeletePictureDatabaseManager()
+        {
+            API = APIFactory.GetAPI("");
+        }
+
+        public void DeletePicture()
+        {
+
+        }
     }
 }
