bc758e4a2e75229cb09456b649b45f7b08867de6
JacobJH <60734597+JacobJH@users.noreply.github.com>
2021-05-18 14:46:11 +0200

    ChangeFollowUpNotification Controller & databaseManager added

---
+++ BusinessLogicLayer/ChangeFollowUpNotificationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccessLayer;
+using DataAccessLayer.DataAccessLayerInterfaces;
+using DataClasses.Domain;
+
+namespace BusinessLogicLayer
+{
+    public class ChangeFollowUpNotificationController : IChangeFollowUpNotificationDatabaseManager
+    {
+        private IChangeFollowUpNotificationDatabaseManager changeFollowUpNotificationDatabaseManager;
+        public ChangeFollowUpNotificationController()
+        {
+            changeFollowUpNotificationDatabaseManager = new ChangeFollowUpNotificationDatabaseManager("");
+        }
+        public void ChangeFollowUpNotification(ChangeNotificationsDomain changeNotificationsDomain)
+        {
+            changeFollowUpNotificationDatabaseManager.ChangeFollowUpNotification(changeNotificationsDomain);
+        }
+    }
+}
---
+++ DTO/Domain/ChangeNotificationsDomain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataClasses.DTO;
+
+namespace DataClasses.Domain
+{
+    public class ChangeNotificationsDomain
+    {
+        public int CollectionID { get; set; }
+        public string NotificationTimeInterval { get; set; }
+        //TODO Outcomment
+        public ChangeNotificationsDTO ToDTO()
+        {
+            ChangeNotificationsDTO changeNotifications = new ChangeNotificationsDTO()
+            {
+                CollectionID = CollectionID,
+                //NotificationTimeInterval = NotificationTimeInterval
+            };
+            return changeNotifications;
+        }
+    }
+}
