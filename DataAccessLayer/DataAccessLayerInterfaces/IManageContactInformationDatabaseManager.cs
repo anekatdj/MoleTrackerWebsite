f5cdb17d442adead5b5db156c024d544cc96f978
JacobJH <60734597+JacobJH@users.noreply.github.com>
2021-05-24 08:37:04 +0200

    ManageContactInfo

---
+++ DataAccessLayer/DataAccessLayerInterfaces/IManageContactInformationDatabaseManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataClasses.Domain;
+using DataClasses.DTO;
+
+namespace DataAccessLayer.DataAccessLayerInterfaces
+{
+    public interface IManageContactInformationDatabaseManager
+    {
+        DoctorInfoDomain GetDoctorInfo(DoctorContactInfoRequestDTO doctorContactInfoRequestDTO);
+    }
+}
---
+++ DataAccessLayer/ManageContactInformationDatabaseManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using APIWebServiesConnector;
+using DataAccessLayer.DataAccessLayerInterfaces;
+using DataClasses.Domain;
+using DataClasses.DTO;
+
+namespace DataAccessLayer
+{
+    public class ManageContactInformationDatabaseManager : IManageContactInformationDatabaseManager
+    {
+
+        private ChangeNotificationsDTO changeNotificationsDTO;
+        private DoctorContactInfoRequestDTO doctorContactInfoRequestDTO;
+        public IAPIService API { get; set; }
+
+        public ManageContactInformationDatabaseManager(string stub)
+        {
+            API = APIFactory.GetAPI(stub);
+        }
+
+        public void ChangeFollowUpNotification(ChangeNotificationsDomain changeNotificationsDomain)
+        {
+            changeNotificationsDTO = changeNotificationsDomain.ToDTO();
+            try
+            {
+                var result =
+                    API.PostObject<ChangeNotificationsDTO>("ChangeNotificationInterval", changeNotificationsDTO);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public DoctorInfoDomain GetDoctorInfo(DoctorContactInfoRequestDTO doctorContactInfoRequestDTO)
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+}
